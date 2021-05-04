c9b4a69c02cd464f08c7fcd62b84100c47f517dc
Sabbir <sabbirahmad49@gmail.com>
2021-05-04 03:41:37 -0400

    changes from USADEV pushed to m so INDIADEV can pull it

--- src/main/java/USA/usaDev/browser.java
+++ src/main/java/USA/usaDev/browser.java
@@ -10,26 +10,19 @@ public class browser {
 
 		System.out.println("log in");
 		
-<<<<<<< HEAD
-		
-=======
 
-
->>>>>>> ccac187738f7bbd18bb7ce8af6a0701c1c04ed2d
-		
 		System.out.println("add to cart");
 		
 		System.out.println("payment");
 		
 		System.out.println("log out");
-<<<<<<< HEAD
-		}
-=======
+
 		
 		System.out.println("return to homepage");
+		
+		System.out.println("shipment");
 		}
 
->>>>>>> ccac187738f7bbd18bb7ce8af6a0701c1c04ed2d
 	
 	
 
