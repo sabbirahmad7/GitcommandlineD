ccac187738f7bbd18bb7ce8af6a0701c1c04ed2d
Sabbir <sabbirahmad49@gmail.com>
2021-05-04 03:12:21 -0400

    new development INDIADEV3

--- src/main/java/USA/usaDev/browser.java
+++ src/main/java/USA/usaDev/browser.java
@@ -7,11 +7,23 @@ public class browser {
 		
 		System.out.print("browser fb");
 		
+
 		System.out.println("log in");
 		
+
+
 		
-	}
+		System.out.println("add to cart");
+		
+		System.out.println("payment");
+		
+		System.out.println("log out");
+		
+		System.out.println("return to homepage");
+		}
+
 	
 	
+
 
 }
